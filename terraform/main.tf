f5bb3d9b3b64767e27aef93929ebe427f550fae1
andras-p <andras@patyi.co>
2022-07-05 10:03:50 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,16 @@ provider "google" {
   region  = var.region
 }
 
-# Cloud Storage bucket
+# a bucket
 resource "google_storage_bucket" "function_bucket" {
   name     = "${lookup(var.project, var.env)}-terratest"
+  location = var.region
+  labels   = var.labels
+}
+
+# other bucket
+resource "google_storage_bucket" "other_bucket" {
+  name     = "${lookup(var.project, var.env)}-terratest2"
   location = var.region
   labels   = var.labels
 }
